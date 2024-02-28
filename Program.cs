45ed1b8460d7c95f2633d857d1e1bbded47cdded
leesuper-learn <161559713+leesuper-learn@users.noreply.github.com>
2024-02-28 16:36:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace systemtime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("Datetime Now is :" + localDate);
+            Console.ReadKey();
+        }
+    }
+}
